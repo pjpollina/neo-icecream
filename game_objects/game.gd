6d13ca376f47411bfa0a49e7392ca27ff0291d26
PJ Pollina <contact@pjpollina.info>
2023-03-30 23:40:44 -0400

    Made player node part of the game scene

--- game_objects/game.gd
+++ game_objects/game.gd
@@ -16,20 +16,25 @@ func _ready():
   $StatusMenu/Scoops.text = "Scoops: %d" % dodges
 
 func _process(_delta):
+  $Player.position = $Player.get_global_mouse_position()
+  $Player.translate(Vector2(-32, -32))
+  $Player.position.x = clamp($Player.position.x, 0, 600-64)
+  $Player.position.y = clamp($Player.position.y, 0, 600-64)
   $BonusPopup/Text.modulate.a = $BonusPopup/Timer.time_left
 
-# Called when the player is hit by a bad projectile
-func _on_player_splattered():
-  $Shooter.clear_shots()
-  lives -= 1
-  Audio.play_sfx("splatter")
+# Called when player is hit
+func _on_player_area_entered(area):
+  if area as Scoop:
+    $Shooter.clear_shots()
+    lives -= 1
+    Audio.play_sfx("splatter")
 
-  $StatusMenu/Lives.text = "Lives: %d" % lives
+    $StatusMenu/Lives.text = "Lives: %d" % lives
 
-  # Trigger a game over when the player is out of lives
-  if(lives < 1):
-    $Shooter.set_process(false)
-    get_tree().change_scene_to_file("res://game_objects/game_over.tscn")
+    # Trigger a game over when the player is out of lives
+    if(lives < 1):
+      $Shooter.set_process(false)
+      get_tree().change_scene_to_file("res://game_objects/game_over.tscn")
 
 # Called after a projectile leaves the screen
 func _on_scoop_outta_here(topping):
