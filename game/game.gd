abb0940915ec7d2396dda3df3c765f2216b62831
PJ Pollina <contact@pjpollina.info>
2023-03-31 15:53:43 -0400

    First chunk of cleanup done

--- game/game.gd
+++ game/game.gd
@@ -1,9 +1,19 @@
 ## Main game class
 extends Node
 
+const scoop = preload("res://game/objects/scoop.tscn")
+
+## GAME STATE ########################################
+##
 var lives:  int  # The player's life count
 var score:  int  # Current score
 var dodges: int  # Scoops successfully dodged
+##
+## SHOOTER STATE #####################################
+##
+var loading:  int = 0       # State of loader (9 = Done)
+var shooting: bool = false  # Is shooter shooting?
+var last_shot: int = -1     # Most recent firing position
 
 # Initialize the state of the game
 func _ready():
@@ -11,19 +21,12 @@ func _ready():
   score = 0
   dodges = 0
 
-  $StatusMenu/Lives.text = "Lives: %d" % lives
-  $StatusMenu/Score.text = "Score: %d" % score
-  $StatusMenu/Scoops.text = "Scoops: %d" % dodges
-
-func _process(delta):
+func _process(_delta):
   $Player.position = $Player.get_global_mouse_position()
   $Player.translate(Vector2(-32, -32))
   $Player.position.x = clamp($Player.position.x, 0, 600-64)
   $Player.position.y = clamp($Player.position.y, 0, 600-64)
   $BonusPopup/Text.modulate.a = $BonusPopup/Timer.time_left
-
-  if loading: process_loader(delta)
-  else: process_shooter(delta)
 
 # Called when player is hit
 func _on_player_area_entered(area):
@@ -31,13 +34,7 @@ func _on_player_area_entered(area):
     clear_shots()
     lives -= 1
     Audio.play_sfx("splatter")
-
-    $StatusMenu/Lives.text = "Lives: %d" % lives
-
-    # Trigger a game over when the player is out of lives
-    if(lives < 1):
-      $Shooter.set_process(false)
-      get_tree().change_scene_to_file("res://menus/game_over.tscn")
+    update_status_menu()
 
 # Called after a projectile leaves the screen
 func _on_scoop_outta_here(topping):
@@ -53,87 +50,92 @@ func _on_scoop_outta_here(topping):
 func _on_timer_timeout():
   $BonusPopup/Timer.stop()
 
+# Remove all shot instances from the holder
+func clear_shots() -> void:
+  for child in $Scoops.get_children():
+    child.queue_free()
+  shooting = false
+  $Shooter/ClearCooldown.start()
+
+## SHOOTER ###########################################
+##
+
+func _on_shot_cooldown_timeout():
+  if !shooting: return
+
+  # Slightly randomize cooldown for next shot
+  $Shooter/ShotCooldown.wait_time = 0.25 + randf_range(-0.5, 0.5)
+
+  # Randomly choose which position to fire from
+  var pos = randi_range(0, 8)
+
+  # Skip firing this cycle if it would repeat the previous position
+  if pos == last_shot:
+    last_shot = -1
+    return
+  else:
+    last_shot = pos
+
+  # Create the shot instance
+  var shot = scoop.instantiate()
+  shot.prepare(1.0, Scoop.Flavor.Strawberry)
+
+  # Put the shot in the right spot
+  shot.position = $Shooter/Positions.get_point_position(pos)
+  shot.translate($Shooter.position)
+
+  # FIRE!!!
+  $Scoops.add_child(shot)
+
+func _on_clear_cooldown_timeout():
+  # Trigger a game over when the player is out of lives
+  if(lives < 1):
+    Audio.play_sfx("ui_popup_show")
+    get_tree().change_scene_to_file("res://menus/game_over.tscn")
+
+  # Otherwise, resume
+  shooting = true
+  $Shooter/ClearCooldown.stop()
+
+## LOADER ############################################
+##
+
+func _on_load_delay_timeout():
+  # Remove the timer once loading is done
+  if(loading >= $Shooter/Positions.points.size()):
+    $Shooter/LoadDelay.queue_free()
+    show_level_popup()
+    return
+
+  # Load up the image
+  var sprite = $Shooter/Scoops/Base.duplicate()
+  sprite.position = $Shooter/Positions.points[loading]
+  $Shooter/Scoops.add_child(sprite)
+  sprite.show()
+
+  # Play the sound
+  Audio.play_sfx("loading_cone", 0.1)
+
+  # Increment
+  loading += 1
+
+## STATUS MENU #######################################
+##
+
+func update_status_menu():
+  $StatusMenu/Lives.text = "Lives: %d" % lives
+  $StatusMenu/Score.text = "Score: %d" % score
+  $StatusMenu/Scoops.text = "Scoops: %d" % dodges
+
+## LEVEL POPUP #######################################
+##
+
+func show_level_popup():
+  $LevelPopup.show()
+  Audio.play_sfx("ui_popup_show")
+
 func _on_level_popup_gui_input(event):
   if event as InputEventMouseButton:
     $LevelPopup.hide()
     Audio.play_sfx("ui_popup_hide")
-    prepare()
-
-const scoop = preload("res://game/objects/scoop.tscn")
-
-var firerate: float = 0.25  # Base time in seconds between shots
-var cooldown: float = -999  # Delta timer
-var last_shot: int = -1     # Most recent firing position
-
-# Where the magic happens
-func process_shooter(delta):
-  # Update the cooldown timer
-  cooldown += delta
-
-  # Where the fun begins
-  if cooldown > firerate:
-    cooldown = 0.0 + randf_range(-0.5, 0.0) # Slightly randomized
-
-    # Randomly choose which position to fire from
-    var pos = randi_range(0, 8)
-
-    # Skip firing this cycle if it would repeat the previous position
-    if pos == last_shot:
-      last_shot = -1
-      return
-    else:
-      last_shot = pos
-
-    # Create the shot instance
-    var shot = scoop.instantiate()
-    shot.prepare(1.0, Scoop.Flavor.Strawberry)
-
-    # Put the shot in the right spot
-    shot.position = $Shooter/Positions.get_point_position(pos)
-    shot.translate($Shooter.position)
-
-    # FIRE!!!
-    $Shooter/ShotHolder.add_child(shot)
-
-# Called to start and/or reset the shooter
-func prepare() -> void:
-  firerate  = 0.25
-  cooldown  = -1.5
-  last_shot = -1
-
-# Remove all shot instances from the holder
-func clear_shots() -> void:
-  for child in $Shooter/ShotHolder.get_children():
-    child.queue_free()
-  prepare()
-
-var index: int = 0
-var loading: bool = true
-
-var load_firerate: float = 0.1  # Cooldown between loads
-var load_cooldown: float = 0.0  # Delta timer
-
-func process_loader(delta):
-  # Update the cooldown timer
-  load_cooldown += delta
-
-  # Where the fun begins
-  if load_cooldown > load_firerate:
-    load_cooldown = 0.0
-
-    if(index >= $Shooter/Positions.points.size()):
-      loading = false
-      $LevelPopup.show()
-      Audio.play_sfx("ui_popup_show")
-      return
-
-    var sprite = $Shooter/LoadSequence/Sprite.duplicate()
-    #sprite.texture = $Sprite.texture
-    sprite.position = $Shooter/Positions.points[index]
-    #sprite.translate(get_parent().position)
-    $Shooter/Scoops.add_child(sprite)
-    sprite.show()
-
-    # Play the sound
-    Audio.play_sfx("loading_cone", 0.1)
-    index += 1
+    shooting = true
